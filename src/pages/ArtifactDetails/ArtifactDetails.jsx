7f624d277d01d977321baff945019e2f26f448d7
akibur-r <akibur.pciu@gmail.com>
2025-06-17 20:03:49 +0600

    Updated styles for better responsive view in details page

--- src/pages/ArtifactDetails/ArtifactDetails.jsx
+++ src/pages/ArtifactDetails/ArtifactDetails.jsx
@@ -42,138 +42,145 @@ const ArtifactDetails = () => {
       {artifactLoading ? (
         <LoaderLogoSpinner />
       ) : (
-        <div className="relative z-0 max-w-screen-xl mx-auto py-10 px-4 grid grid-cols-2 gap-x-16">
-          <div className="">
-            <img
-              src={artifact.imageURL}
-              className="relative h-full w-full object-contain h-50vh md:max-h-[60vh] rounded-xs"
-              alt=""
-            />
-          </div>
-
-          <div className="space-y-4">
-            {/* name */}
-            <div>
-              <h1 className="text-5xl font-cinzel font-medium">
-                {artifact.name}
-              </h1>
+        <div className="max-w-screen-xl mx-auto py-10 px-4 ">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-x-16 gap-y-6">
+            <div className="">
+              <img
+                src={artifact.imageURL}
+                className="relative w-full object-contain max-h-[40vh] md:max-h-[60vh] rounded-xs"
+                alt=""
+              />
             </div>
 
-            {/* small details */}
-            <div className="flex gap-6 items-center w-7/12 h-fit ">
-              <h4>
-                <span className="text-accent">{artifact.type}</span>
-              </h4>
-
-              <div className="h-4">
-                <Separator
-                  orientation="vertical"
-                  className="bg-accent h-full opacity-70"
-                />
+            <div className="space-y-2 md:space-y-4">
+              {/* name */}
+              <div className="text-center md:text-left">
+                <h1 className="text-3xl lg:text-5xl font-cinzel font-medium">
+                  {artifact.name}
+                </h1>
               </div>
 
-              <h4 className="text-sm ">
-                <span className="text-accent">Created at</span>{" "}
-                <span className="font-medium text-base-content">
-                  {artifact.createdAt}
-                </span>
-              </h4>
-            </div>
+              {/* small details */}
+              <div className="flex gap-3 md:gap-4 lg:gap-6 justify-center md:justify-start items-center md:w-7/12 h-fit ">
+                <h4>
+                  <span className="text-accent">{artifact.type}</span>
+                </h4>
 
-            {/* context */}
-            <div>
-              <p className="opacity-80">{artifact.historicalContext}</p>
-            </div>
+                <div className="h-4">
+                  <Separator
+                    orientation="vertical"
+                    className="bg-foreground h-full opacity-40"
+                  />
+                </div>
 
-            {/* Like button */}
-            <div className="space-y-1">
+                <h4 className="text-sm ">
+                  <span className="text-accent">Created at</span>{" "}
+                  <span className="font-medium text-base-content">
+                    {artifact.createdAt}
+                  </span>
+                </h4>
+              </div>
+
+              {/* context */}
               <div>
+                <p className="opacity-80 text-center md:text-left">
+                  {artifact.historicalContext}
+                </p>
+              </div>
+
+              {/* Like button */}
+              <div className="flex flex-col md:items-start items-center py-2 md:py-0 gap-y-1">
+                <div>
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <Button
+                        variant={"secondary"}
+                        size={"sm"}
+                        className="rounded-xs bg-green-500/5 hover:bg-green-500/5 hover:text-green-500 cursor-pointer text-base-content border border-green-500/20"
+                      >
+                        <BiLike />
+                        <span>{artifact.likeCount} Likes</span>
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>Like This Artifact</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </div>
+                <div className="text-xs">you did not like this artifact.</div>
+              </div>
+
+              <Separator />
+
+              {/* other details */}
+              <div className="flex justify-center md:justify-start">
+                <div className="grid grid-cols-2 md:grid-cols-1 gap-y-1 gap-x-3">
+                  <div>
+                    <span className="flex items-center gap-2 overflow-hidden">
+                      <GiDigDug className="text-md md:text-lg opacity-90 shrink-0 text-accent" />{" "}
+                      {artifact.discoveredBy}
+                    </span>
+                  </div>
+                  <div>
+                    <span className="flex items-center gap-2 overflow-hidden">
+                      <LuSearchCheck className="text-md md:text-lg opacity-90 text-accent" />{" "}
+                      {artifact.discoveredAt}
+                    </span>
+                  </div>
+                  <div>
+                    <span className="flex items-center gap-2 overflow-hidden">
+                      <GrLocation className="text-md md:text-lg opacity-90 text-accent" />{" "}
+                      {artifact.presentAddress}
+                    </span>
+                  </div>
+                  <div>
+                    <span className="flex items-center gap-2 overflow-hidden">
+                      <AiOutlineAppstoreAdd className="text-md md:text-lg opacity-90 text-accent" />{" "}
+                      {artifact.userName}
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              {/* <Separator className="hidden md:block" /> */}
+
+              {/* action buttons: HIDDEN IF OWNER IS SOMEONE ELSE */}
+              {/* <div className="flex gap-2 justify-center md:justify-start py-2"> */}
+              <div className="hidden">
+                {/* update */}
                 <Tooltip>
                   <TooltipTrigger>
                     <Button
                       variant={"secondary"}
                       size={"sm"}
-                      className="rounded-xs bg-green-500/5 hover:bg-green-500/5 hover:text-green-500 cursor-pointer text-base-content border border-green-500/20"
+                      className="rounded-xs bg-yellow-500/50 hover:bg-yellow-500/15 hover:text-yellow-500 cursor-pointer text-base-content border border-yellow-500/20"
                     >
-                      <BiLike />
-                      <span>{artifact.likeCount} Likes</span>
+                      <BiEditAlt />
+                      <span>Update</span>
                     </Button>
                   </TooltipTrigger>
                   <TooltipContent>
-                    <p>Like This Artifact</p>
+                    <p>Edit This Artifact</p>
+                  </TooltipContent>
+                </Tooltip>
+
+                {/* delete */}
+                <Tooltip>
+                  <TooltipTrigger>
+                    <Button
+                      variant={"secondary"}
+                      size={"sm"}
+                      className="rounded-xs bg-red-500/50 hover:bg-red-500/15 hover:text-red-500 cursor-pointer text-base-content border border-red-500/20"
+                    >
+                      <BiTrashAlt />
+                      <span>Delete</span>
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Delete This Artifact</p>
                   </TooltipContent>
                 </Tooltip>
               </div>
-              <div className="text-xs">you did not like this artifact.</div>
-            </div>
-
-            <Separator />
-
-            {/* other details */}
-            <div className="space-y-1">
-              <div>
-                <span className="flex items-center gap-2">
-                  <GiDigDug className="text-lg opacity-90 text-accent" />{" "}
-                  {artifact.discoveredBy}
-                </span>
-              </div>
-              <div>
-                <span className="flex items-center gap-2">
-                  <LuSearchCheck className="text-lg opacity-90 text-accent" />{" "}
-                  {artifact.discoveredAt}
-                </span>
-              </div>
-              <div>
-                <span className="flex items-center gap-2">
-                  <GrLocation className="text-lg opacity-90 text-accent" />{" "}
-                  {artifact.presentAddress}
-                </span>
-              </div>
-              <div>
-                <span className="flex items-center gap-2">
-                  <AiOutlineAppstoreAdd className="text-lg opacity-90 text-accent" />{" "}
-                  {artifact.userName}
-                </span>
-              </div>
-            </div>
-
-            <Separator />
-
-            {/* action buttons */}
-            <div className="flex gap-2">
-              {/* update */}
-              <Tooltip>
-                <TooltipTrigger>
-                  <Button
-                    variant={"secondary"}
-                    size={"sm"}
-                    className="rounded-xs bg-yellow-500/50 hover:bg-yellow-500/15 hover:text-yellow-500 cursor-pointer text-base-content border border-yellow-500/20"
-                  >
-                    <BiEditAlt />
-                    <span>Update</span>
-                  </Button>
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p>Edit This Artifact</p>
-                </TooltipContent>
-              </Tooltip>
-
-              {/* delete */}
-              <Tooltip>
-                <TooltipTrigger>
-                  <Button
-                    variant={"secondary"}
-                    size={"sm"}
-                    className="rounded-xs bg-red-500/50 hover:bg-red-500/15 hover:text-red-500 cursor-pointer text-base-content border border-red-500/20"
-                  >
-                    <BiTrashAlt />
-                    <span>Delete</span>
-                  </Button>
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p>Delete This Artifact</p>
-                </TooltipContent>
-              </Tooltip>
             </div>
           </div>
         </div>
