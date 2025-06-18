2213c29798a1713c204ce625d85745d022ee64d6
akibur-r <akibur.pciu@gmail.com>
2025-06-18 18:31:36 +0600

    Made aftifact delete button more functional

--- src/components/shared/ArtifactDeleteButton/ArtifactDeleteButton.jsx
+++ src/components/shared/ArtifactDeleteButton/ArtifactDeleteButton.jsx
@@ -19,28 +19,55 @@ import useArtifactsApi from "@/api/artifactsApi";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { BiTrashAlt } from "react-icons/bi";
-import { useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router";
+import { toast } from "sonner";
+import LoaderSpinner from "../LoaderSpinner/LoaderSpinner";
 
 const ArtifactDeleteButton = ({ artifact, showText = true }) => {
   const { deleteArtifactPromise } = useArtifactsApi();
   const navigate = useNavigate();
   const [open, setOpen] = useState(false);
+  const [deleteBtnLoading, setDeleteBtnLoading] = useState(false);
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const location = useLocation();
 
   const handleDeleteArtifact = () => {
+    setDeleteBtnLoading(true);
     deleteArtifactPromise(artifact._id)
       .then((res) => {
-        console.log(res);
-        navigate("/all-artifacts");
+        if (res.deletedCount) {
+          toast.success("Deleted", {
+            description: "The artifact is removed from the server.",
+          });
+          setDeleteBtnLoading(false);
+          if (location.pathname !== "/my-artifacts") {
+            navigate("/all-artifacts");
+          } else {
+            navigate("/my-artifacts");
+          }
+          setDialogOpen(false);
+        } else {
+          toast.error("Not Deleted", {
+            description: "There was an error while deleting this artifact.",
+          });
+          setDeleteBtnLoading(false);
+          setDialogOpen(false);
+        }
       })
-      .catch((err) => {
-        console.log(err);
+      .catch(() => {
+        toast.error("Not Deleted", {
+          description: "There was an error while deleting this artifact.",
+        });
+        setDeleteBtnLoading(false);
+        setDialogOpen(false);
       });
   };
 
   return (
     <Tooltip>
       <TooltipTrigger>
-        <Dialog>
+        <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
           <DialogTrigger>
             <Button
               variant={"secondary"}
@@ -71,7 +98,7 @@ const ArtifactDeleteButton = ({ artifact, showText = true }) => {
                 variant={"default"}
                 className="rounded-xs bg-red-500/70 hover:bg-red-500/80 text-base-content"
               >
-                Delete
+                {deleteBtnLoading ? <LoaderSpinner /> : "Delete"}
               </Button>
             </DialogFooter>
           </DialogContent>
