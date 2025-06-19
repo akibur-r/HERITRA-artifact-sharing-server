54cd9b7be876a445500a0436f0f9184f842c17ff
akibur-r <akibur.pciu@gmail.com>
2025-06-20 02:34:02 +0600

    Added request interceptor to send auth header

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,12 +1,19 @@
 import axios from "axios";
+import useAuth from "./useAuth";
 
 const axiosInstance = axios.create({
-  baseURL:
-    "http://localhost:3000",
-    // "https://heritra-server.vercel.app",
+  baseURL: "http://localhost:3000",
+  // "https://heritra-server.vercel.app",
 });
 
 const useAxiosSecure = () => {
+  const { user } = useAuth();
+
+  axiosInstance.interceptors.request.use((config) => {
+    config.headers.authorization = `Bearer ${user.accessToken}`;
+    return config;
+  });
+
   return axiosInstance;
 };
 
